70937ff13c05ad592d1e17370c314ccef027caa7
Govardhan8 <govardhanmurthy007@gmail.com>
2021-10-21 00:31:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+URL to get initials Pokémon details:
+https://pokeapi.co/api/v2/pokemon?limit=50
+
+URL to get abilities, name, moves and weight:
+https://pokeapi.co/api/v2/pokemon/2/
+
+URL to get image of Pokémon:
+https://pokeapi.co/api/v2/pokemon-form/1/
+
+Google Fonts Used:
+Name-font: 'Rampart One'
+Number-font: 'Abril Fatface'
+Header-font: 'Domine'
+Details-font: 'Fjalla One'
+
+About the project:
+The app has 60 Pokemon cards, which contains the data rendered from the above mentioned api's.
+
+HTML part(index.html):
+The app only has a script tag for loading the script.js file.
+Rest of the content is rendered using only the DOM
+
+CSS part(style.css):
+The app is mainly constructed using css grids.
+
+JS part(script.js):
+The javascript part of the app is done with async,fetch and await for getting the data from the api.
+The rendering part to the dom is done by using innerHTML and template literals.
---
+++ script.js
@@ -0,0 +1,84 @@
+//Fetching image and details of the pokemons
+const fetchDetails = async (id) => {
+	//Using try catch to catch errors if any at the first api calling point
+	try {
+		//Image data from the api
+		const imageData = await fetch(
+			`https://pokeapi.co/api/v2/pokemon-form/${id}/`
+		)
+		const image = await imageData.json()
+
+		//Pokemon details from the api
+		const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+		const details = await data.json()
+
+		//Returning the details and image data as an object
+		return { details, image }
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+//Making both image and details as a single pokemon object and making an array of pokemons
+const renderPokemon = async () => {
+	const pokemons = []
+	for (let i = 1; i <= 60; i++) {
+		//Getting the pokemon data
+		const pokemon = await fetchDetails(i)
+		pokemons.push(pokemon)
+	}
+	return pokemons
+}
+
+//Rendering the Pokemons onto the screen
+const pageRender = async () => {
+	//Getting the array of pokemon objects
+	const Pokemons = await renderPokemon()
+
+	let cards = ''
+	//Rendering the array of pokemon objects onto the screen
+	Pokemons.forEach((pokemon) => {
+		cards += `
+        <section class="cardContainer">
+	    <article class="card">
+	                <img class="image" src="${pokemon.image.sprites.front_default}" alt="${pokemon.details.name}" />
+                    <p class="name">${pokemon.details.name}</p>        
+                    <p class="weight"> 
+                        <span>Weight</span>
+                        ${pokemon.details.weight} 
+                    </p>   
+        </article>
+        <article class="details">
+            <div>
+                <span>Abilities</span>
+                <p>
+                ${pokemon.details.abilities[0]?.ability.name} ,
+                ${pokemon.details.abilities[1]?.ability.name}
+                </p>
+                <span>Moves</span>
+                <p>               
+                    ${pokemon.details.moves[0].move.name},
+                    ${pokemon.details.moves[1]?.move.name},
+                    ${pokemon.details.moves[2]?.move.name},
+                    ${pokemon.details.moves[3]?.move.name},
+                    ${pokemon.details.moves[4]?.move.name},
+                    ${pokemon.details.moves[5]?.move.name}
+                </p>                     
+            </div>
+        </article>
+        </section>
+	`
+	})
+
+	document.body.innerHTML = `
+    <header>
+        <h1>POKEMON API</h1>
+    </header>
+    <main class="container">
+        ${cards}
+    </main>
+    `
+}
+
+//Calling the render method
+pageRender()
